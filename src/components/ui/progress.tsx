c569eacc270a80918cdcb3d23932f2dbdc3d8048
Nisha Chavan <nisha.chavan@lekab.com>
2025-10-08 10:51:45 +0200

    COLOR IN PROGRESS

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -19,14 +19,14 @@ const Progress = React.forwardRef<
         <ProgressPrimitive.Root
             ref={ref}
             className={cn(
-                "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
+                "relative h-2 w-full overflow-hidden rounded-full bg-slate-700/30",
                 className
             )}
             value={value}
             {...props}
         >
             <ProgressPrimitive.Indicator
-                className="h-full w-full flex-1 bg-primary transition-all"
+                className="h-full w-full flex-1 bg-cyan-500 transition-all"
                 style={{ transform: `translateX(-${100 - pct}%)` }}
             />
         </ProgressPrimitive.Root>
